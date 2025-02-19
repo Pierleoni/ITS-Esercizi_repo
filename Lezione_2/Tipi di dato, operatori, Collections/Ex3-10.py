9de44674b662daa464bdd555aee43b7d6e986598
Marco <mpierleo@gmail.com>
2025-02-19 13:07:42 +0100

    6th commit

---
+++ Lezione_2/Tipi di dato, operatori, Collections/Ex3-10.py
@@ -0,0 +1,8 @@
+# Create a nested list of places
+list_places = [
+    ["Mountains", ["Everest", "K2", "Kangchenjunga"]],
+    ["Rivers", ["Amazon", "Nile", "Yangtze"]],
+    ["Countries", ["Italy", "Canada", "Japan"]],
+    ["Cities", ["New York", "Tokyo", "Paris"]],
+    ["Languages", ["English", "Spanish", "Mandarin"]]
+]
--- Lezione_2/Tipi di dato, operatori, Collections/Ex3-7.py
+++ Lezione_2/Tipi di dato, operatori, Collections/Ex3-7.py
@@ -10,12 +10,12 @@ print(f"Unfortunately {list_person[3]} the table won't arrive in time for the di
 print(f"Unfortunately {list_person[4]} the table won't arrive in time for the dinner")
 print(f"Unfortunately {list_person[5]} the table won't arrive in time for the dinner")
 print(f"Unfortunately {list_person[6]} the table won't arrive in time for the dinner")
-list_person.pop(6)
-list_person.pop(2)
-list_person.pop(0)
-list_person.pop(3)
-list_person.pop(2)
-print(list_person)
+print(f"Unfortunately {list_person.pop(6)} are not more invited")
+print(f"Unfortunately {list_person.pop(2)} are not more invited")
+print(f"Unfortunately {list_person.pop(0)} are not more invited")
+print(f"Unfortunately {list_person.pop(3)} are not more invited")
+print(f"Unfortunately {list_person.pop(2)} are not more invited")
+# print(list_person) 
 # print(f"I'd like to informed Mr.{list_person[0]} that you are still on the list of the guests for the dinner")
 # print(f"I'd like to informed Mr.{list_person[1]} that you are still on the list of the guests for the dinner")
 # del list_person[1]
@@ -46,5 +46,5 @@ cont = 0
 len= len(list_person)
 while cont<len:
     print(f"I'd like to informed Mr.{list_person[cont]} that you are still on the list of the guests for the dinner")
-    print(f"I'd like to informed Mr.{list_person[cont]} that you are still on the list of the guests for the dinner")
+    
     cont+=1
